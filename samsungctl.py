356dea36b7b9db115f8e2ffcf7f46dbcb21330ae
Lauri Niskanen <ape@ape3000.com>
2014-03-14 10:36:55 +0200

    Read settings from a configuration file

--- samsungctl.py
+++ samsungctl.py
@@ -2,14 +2,17 @@
 
 import argparse
 import base64
+import collections
+import json
+import os
 import socket
 import sys
 import time
 
 class Remote():
-	def __init__(self, addr, port, name, description, id):
+	def __init__(self, host, port, name, description, id):
 		self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.connection.connect((addr, port))
+		self.connection.connect((host, port))
 
 		payload = b"\x64\x00" +\
 		          self._serialize_string(str.encode(description)) +\
@@ -44,17 +47,52 @@ class Remote():
 
 		return bytes([len(string)]) + b"\x00" + string
 
+def read_config():
+	config = collections.defaultdict(lambda: None, {
+		"name": "samsungctl",
+		"description": "PC",
+		"id": "",
+		"port": 55000,
+	})
+
+	config_directory = os.getenv("XDG_CONFIG_HOME") or os.path.join(os.getenv("HOME"), ".config")
+
+	try:
+		config_file = open(os.path.join(config_directory, "samsungctl.conf"))
+	except FileNotFoundError:
+		return config
+
+	with config_file:
+		try:
+			config_json = json.load(config_file)
+		except ValueError as e:
+			print("Warning: Could not parse the configuration file.")
+			print(e)
+			print()
+			return config
+
+		config.update(config_json)
+
+	return config
+
 if __name__ == "__main__":
+	config = read_config()
+
 	parser = argparse.ArgumentParser(description="Remote control Samsung televisions via TCP/IP connection.",
-	                                 epilog="E.g. {0} --name myremote 192.168.0.10 KEY_VOLDOWN".format(sys.argv[0]))
-	parser.add_argument("ip", help="TV IP address")
+	                                 epilog="E.g. {0} --host 192.168.0.10 --name myremote KEY_VOLDOWN".format(sys.argv[0]))
 	parser.add_argument("key", help="key to be sent (e.g. KEY_VOLDOWN)")
-	parser.add_argument("--name", default="samsungctl", help="remote control name")
-	parser.add_argument("--description", default="PC", help="remote control description")
-	parser.add_argument("--id", default="", help="remote control id")
-	parser.add_argument("--port", type=int, default=55000, help="TV port number (TCP)")
+	parser.add_argument("--host", default=config["host"], help="TV hostname or IP address")
+	parser.add_argument("--name", default=config["name"], help="remote control name")
+	parser.add_argument("--description", default=config["description"], help="remote control description")
+	parser.add_argument("--id", default=config["id"], help="remote control id")
+	parser.add_argument("--port", type=int, default=config["port"], help="TV port number (TCP)")
 
 	args = parser.parse_args()
 
-	with Remote(args.ip, args.port, args.name, args.description, args.id) as remote:
+	if not args.host:
+		print("Error: --host must be set")
+		parser.print_help()
+		sys.exit()
+
+	with Remote(args.host, args.port, args.name, args.description, args.id) as remote:
 		remote.control(args.key)
