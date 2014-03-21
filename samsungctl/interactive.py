549f4362956dd0b14ec092cfc5845aaa91b8c7dd
Lauri Niskanen <ape@ape3000.com>
2014-03-21 19:41:25 +0200

    Print key mappings in interactive mode

--- samsungctl/interactive.py
+++ samsungctl/interactive.py
@@ -1,49 +1,55 @@
 import curses
 
-_mappings = {
-	"p":    "KEY_POWEROFF",
-	"KEY_UP":    "KEY_UP",
-	"KEY_DOWN":  "KEY_DOWN",
-	"KEY_LEFT":  "KEY_LEFT",
-	"KEY_RIGHT": "KEY_RIGHT",
-	"KEY_PPAGE": "KEY_CHUP",
-	"KEY_NPAGE": "KEY_CHDOWN",
-	"\n":        "KEY_ENTER",
-	"\x7f":      "KEY_RETURN",
-	"l":         "KEY_CH_LIST",
-	"m":         "KEY_MENU",
-	"s":         "KEY_SOURCE",
-	"g":         "KEY_GUIDE",
-	"t":         "KEY_TOOLS",
-	"i":         "KEY_INFO",
-	"z":         "KEY_RED",
-	"x":         "KEY_GREEN",
-	"c":         "KEY_YELLOW",
-	"v":         "KEY_BLUE",
-	"d":         "KEY_PANNEL_CHDOWN", # 3D
-	"+":         "KEY_VOLUP",
-	"-":         "KEY_VOLDOWN",
-	"*":         "KEY_MUTE",
-	"0":         "KEY_0",
-	"1":         "KEY_1",
-	"2":         "KEY_2",
-	"3":         "KEY_3",
-	"4":         "KEY_4",
-	"5":         "KEY_5",
-	"6":         "KEY_6",
-	"7":         "KEY_7",
-	"8":         "KEY_8",
-	"9":         "KEY_9",
-	"KEY_F(1)":  "KEY_DTV",
-	"KEY_F(2)":  "KEY_HDMI",
-}
+_mappings = [
+	["p",         "KEY_POWEROFF",      "P",         "Power off"],
+	["KEY_UP",    "KEY_UP",            "Up",        "Up"],
+	["KEY_DOWN",  "KEY_DOWN",          "Down",      "Down"],
+	["KEY_LEFT",  "KEY_LEFT",          "Left",      "Left"],
+	["KEY_RIGHT", "KEY_RIGHT",         "Right",     "Right"],
+	["KEY_PPAGE", "KEY_CHUP",          "Page Up",   "P Up"],
+	["KEY_NPAGE", "KEY_CHDOWN",        "Page Down", "P Down"],
+	["\n",        "KEY_ENTER",         "Enter",     "Enter"],
+	["\x7f",      "KEY_RETURN",        "Backspace", "Return"],
+	["l",         "KEY_CH_LIST",       "L",         "Channel List"],
+	["m",         "KEY_MENU",          "M",         "Menu"],
+	["s",         "KEY_SOURCE",        "S",         "Source"],
+	["g",         "KEY_GUIDE",         "G",         "Guide"],
+	["t",         "KEY_TOOLS",         "T",         "Tools"],
+	["i",         "KEY_INFO",          "I",         "Info"],
+	["z",         "KEY_RED",           "Z",         "A / Red"],
+	["x",         "KEY_GREEN",         "X",         "B / Green"],
+	["c",         "KEY_YELLOW",        "C",         "C / Yellow"],
+	["v",         "KEY_BLUE",          "V",         "D / Blue"],
+	["d",         "KEY_PANNEL_CHDOWN", "D",         "3D"],
+	["+",         "KEY_VOLUP",         "+",         "Volume Up"],
+	["-",         "KEY_VOLDOWN",       "-",         "Volume Down"],
+	["*",         "KEY_MUTE",          "*",         "Mute"],
+	["0",         "KEY_0",             "0",         "0"],
+	["1",         "KEY_1",             "1",         "1"],
+	["2",         "KEY_2",             "2",         "2"],
+	["3",         "KEY_3",             "3",         "3"],
+	["4",         "KEY_4",             "4",         "4"],
+	["5",         "KEY_5",             "5",         "5"],
+	["6",         "KEY_6",             "6",         "6"],
+	["7",         "KEY_7",             "7",         "7"],
+	["8",         "KEY_8",             "8",         "8"],
+	["9",         "KEY_9",             "9",         "9"],
+	["KEY_F(1)",  "KEY_DTV",           "F1",        "TV Source"],
+	["KEY_F(2)",  "KEY_HDMI",          "F2",        "HDMI Source"],
+]
 
 def run(remote):
 	curses.wrapper(control, remote)
 
 def control(stdscr, remote):
-	stdscr.addstr("Running interactive mode\n\n")
-	stdscr.addstr("Press 'q' to exit.\n")
+	stdscr.addstr("Interactive mode\n\n", curses.A_BOLD)
+	stdscr.addstr("The key mappings are listed below. Press 'q' to exit.\n")
+
+	column_len = max(len(mapping[2]) for mapping in _mappings) + 1
+	mappings_dict = {}
+	for mapping in _mappings:
+		mappings_dict[mapping[0]] = mapping[1]
+		stdscr.addstr("  {0}= {1}\n".format(mapping[2].ljust(column_len), mapping[3]))
 
 	running = True
 	while running:
@@ -52,6 +58,6 @@ def control(stdscr, remote):
 		if key == "q":
 			running = False
 
-		if key in _mappings:
+		if key in mappings_dict:
 			stdscr.addstr(".")
-			remote.control(_mappings[key])
+			remote.control(mappings_dict[key])
