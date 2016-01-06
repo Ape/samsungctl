8d7635731d758cb1e01fa40366657ad50b50cd22
Lauri Niskanen <ape@ape3000.com>
2016-01-06 12:18:19 +0200

    Bump version to 0.4.0

--- samsungctl/__init__.py
+++ samsungctl/__init__.py
@@ -1,7 +1,7 @@
 """Remote control Samsung televisions via TCP/IP connection"""
 
 __title__        = "samsungctl"
-__version__      = "0.3.0-git"
+__version__      = "0.4.0-git"
 __url__          = "https://github.com/Ape/samsungctl"
 __author__       = "Lauri Niskanen"
 __author_email__ = "ape@ape3000.com"
