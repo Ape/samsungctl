9b4de6b9fc878ddb0aaa9138dee97923a419c1d3
Lauri Niskanen <ape@ape3000.com>
2016-01-07 13:53:29 +0200

    Improve interactive mode UI for small terminals

--- samsungctl/interactive.py
+++ samsungctl/interactive.py
@@ -43,15 +43,23 @@ def run(remote):
     curses.wrapper(_control, remote)
 
 def _control(stdscr, remote):
-    stdscr.addstr("Interactive mode\n\n", curses.A_BOLD)
-    stdscr.addstr("The key mappings are listed below. Press 'q' to exit.\n")
+    height, width = stdscr.getmaxyx()
+
+    stdscr.addstr("Interactive mode, press 'Q' to exit.\n")
+    stdscr.addstr("Key mappings:\n")
 
     column_len = max(len(mapping[2]) for mapping in _mappings) + 1
     mappings_dict = {}
     for mapping in _mappings:
         mappings_dict[mapping[0]] = mapping[1]
-        stdscr.addstr("  {}= {} ({})\n".format(mapping[2].ljust(column_len),
-                                               mapping[3], mapping[1]))
+
+        row = stdscr.getyx()[0] + 2
+        if row < height:
+            line = "  {}= {} ({})\n".format(mapping[2].ljust(column_len),
+                                            mapping[3], mapping[1])
+            stdscr.addstr(line)
+        elif row == height:
+            stdscr.addstr("[Terminal is too small to show all keys]\n")
 
     running = True
     while running:
@@ -61,5 +69,11 @@ def _control(stdscr, remote):
             running = False
 
         if key in mappings_dict:
-            stdscr.addstr(".")
             remote.control(mappings_dict[key])
+
+            try:
+                stdscr.addstr(".")
+            except curses.error:
+                stdscr.deleteln()
+                stdscr.move(stdscr.getyx()[0], 0)
+                stdscr.addstr(".")
