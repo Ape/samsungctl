Ape/samsungctl

commit e5f7f4420213597c826053b2beda544e053d4a55

Add websocket support for 2016 TVs

samsungctl/__main__.py
@@ -9,6 +9,7 @@ from . import __doc__ as doc
 from . import __title__ as title
 from . import __version__ as version
 from . import interactive
+from . import exceptions
 from . import Remote
 
 def _read_config():
@@ -16,7 +17,7 @@ def _read_config():
         "name": "samsungctl",
         "description": "PC",
         "id": "",
-        "port": 55000,
+        "method": "legacy",
         "timeout": 0,
     })
 
@@ -66,6 +67,7 @@ def main():
                         help="interactive control")
     parser.add_argument("--host", help="TV hostname or IP address")
     parser.add_argument("--port", type=int, help="TV port number (TCP)")
+    parser.add_argument("--method", help="Connection method (legacy or websocket)")
     parser.add_argument("--name", help="remote control name")
     parser.add_argument("--description", metavar="DESC",
                         help="remote control description")
@@ -96,19 +98,22 @@ def main():
         return
 
     try:
-        with Remote(config) as remote:
-            for key in args.key:
-                remote.control(key)
+        remote = Remote(config)
 
-            if args.interactive:
-                logging.getLogger().setLevel(logging.ERROR)
-                interactive.run(remote)
-            elif len(args.key) == 0:
-                logging.warning("Warning: No keys specified.")
-    except Remote.ConnectionClosed:
+        for key in args.key:
+            remote.control(key)
+
+        if args.interactive:
+            logging.getLogger().setLevel(logging.ERROR)
+            interactive.run(remote)
+        elif len(args.key) == 0:
+            logging.warning("Warning: No keys specified.")
+    except exceptions.ConnectionClosed:
         logging.error("Error: Connection closed!")
-    except Remote.AccessDenied:
+    except exceptions.AccessDenied:
         logging.error("Error: Access denied!")
+    except exceptions.UnknownMethod:
+        logging.error("Error: Unknown method '{}'".format(config["method"]))
     except socket.timeout:
         logging.error("Error: Timed out!")
     except OSError as e:

samsungctl/remote.py
@@ -1,111 +1,18 @@
-import base64
-import logging
-import socket
-import time
+from . import exceptions
+from .remote_legacy import RemoteLegacy
+from .remote_websocket import RemoteWebsocket
 
-class Remote():
-    """Object for remote control connection."""
-
-    class ConnectionClosed(Exception):
-        """Connection was closed."""
-        pass
-
-    class AccessDenied(Exception):
-        """Connection was denied."""
-        pass
-
-    class UnhandledResponse(Exception):
-        """Received unknown response."""
-        pass
-
+class Remote:
     def __init__(self, config):
-        """Make a new connection."""
-        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        if config["timeout"]:
-            self.connection.settimeout(config["timeout"])
-
-        self.connection.connect((config["host"], config["port"]))
-
-        payload = b"\x64\x00" \
-                  + self._serialize_string(config["description"]) \
-                  + self._serialize_string(config["id"]) \
-                  + self._serialize_string(config["name"])
-        packet = b"\x00\x00\x00" + self._serialize_string(payload, True)
-
-        logging.info("Sending handshake.")
-        self.connection.send(packet)
-        self._read_response(True)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.close()
+        if config["method"] == "legacy":
+            self.remote = RemoteLegacy(config)
+        elif config["method"] == "websocket":
+            self.remote = RemoteWebsocket(config)
+        else:
+            raise exceptions.UnknownMethod()
 
     def close(self):
-        """Close the connection."""
-        if self.connection:
-            self.connection.close()
-            self.connection = None
-            logging.debug("Connection closed.")
+        return self.remote.close()
 
     def control(self, key):
-        """Send a control command."""
-        if not self.connection:
-            raise self.ConnectionClosed()
-
-        payload = b"\x00\x00\x00" + self._serialize_string(key)
-        packet = b"\x00\x00\x00" + self._serialize_string(payload, True)
-
-        logging.info("Sending control command: %s", key)
-        self.connection.send(packet)
-        self._read_response()
-        time.sleep(self._key_interval)
-
-    _key_interval = 0.2
-
-    def _read_response(self, first_time=False):
-        header = self.connection.recv(3)
-        tv_name_len = int.from_bytes(header[1:3],
-                                     byteorder="little")
-        tv_name = self.connection.recv(tv_name_len)
-
-        if first_time:
-            logging.debug("Connected to '%s'.", tv_name.decode())
-
-        response_len = int.from_bytes(self.connection.recv(2),
-                                      byteorder="little")
-        response = self.connection.recv(response_len)
-
-        if len(response) == 0:
-            self.close()
-            raise self.ConnectionClosed()
-
-        if response == b"\x64\x00\x01\x00":
-            logging.debug("Access granted.")
-            return
-        elif response == b"\x64\x00\x00\x00":
-            raise self.AccessDenied()
-        elif response[0:1] == b"\x0a":
-            if first_time:
-                logging.warning("Waiting for authorization...")
-            return self._read_response()
-        elif response[0:1] == b"\x65":
-            logging.warning("Authorization cancelled.")
-            raise self.AccessDenied()
-        elif response == b"\x00\x00\x00\x00":
-            logging.debug("Control accepted.")
-            return
-
-        raise self.UnhandledResponse(response)
-
-    @staticmethod
-    def _serialize_string(string, raw = False):
-        if isinstance(string, str):
-            string = str.encode(string)
-
-        if not raw:
-            string = base64.b64encode(string)
-
-        return bytes([len(string)]) + b"\x00" + string
+        return self.remote.control(key)

samsungctl.conf
@@ -2,6 +2,7 @@
 	"name": "samsungctl",
 	"description": "PC",
 	"id": "",
+	"method": "legacy",
 	"port": 55000,
 	"timeout": 0
 }

samsungctl/exceptions.py
@@ -1,0 +1,15 @@
+class AccessDenied(Exception):
+    """Connection was denied."""
+    pass
+
+class ConnectionClosed(Exception):
+    """Connection was closed."""
+    pass
+
+class UnhandledResponse(Exception):
+    """Received unknown response."""
+    pass
+
+class UnknownMethod(Exception):
+    """Unknown method."""
+    pass

samsungctl/remote_legacy.py
@@ -1,0 +1,104 @@
+import base64
+import logging
+import socket
+import time
+
+from . import exceptions
+
+class RemoteLegacy():
+    """Object for remote control connection."""
+
+    def __init__(self, config):
+        if not config["port"]:
+            config["port"] = 55000
+
+        """Make a new connection."""
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        if config["timeout"]:
+            self.connection.settimeout(config["timeout"])
+
+        self.connection.connect((config["host"], config["port"]))
+
+        payload = b"\x64\x00" \
+                  + self._serialize_string(config["description"]) \
+                  + self._serialize_string(config["id"]) \
+                  + self._serialize_string(config["name"])
+        packet = b"\x00\x00\x00" + self._serialize_string(payload, True)
+
+        logging.info("Sending handshake.")
+        self.connection.send(packet)
+        self._read_response(True)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def close(self):
+        """Close the connection."""
+        if self.connection:
+            self.connection.close()
+            self.connection = None
+            logging.debug("Connection closed.")
+
+    def control(self, key):
+        """Send a control command."""
+        if not self.connection:
+            raise exceptions.ConnectionClosed()
+
+        payload = b"\x00\x00\x00" + self._serialize_string(key)
+        packet = b"\x00\x00\x00" + self._serialize_string(payload, True)
+
+        logging.info("Sending control command: %s", key)
+        self.connection.send(packet)
+        self._read_response()
+        time.sleep(self._key_interval)
+
+    _key_interval = 0.2
+
+    def _read_response(self, first_time=False):
+        header = self.connection.recv(3)
+        tv_name_len = int.from_bytes(header[1:3],
+                                     byteorder="little")
+        tv_name = self.connection.recv(tv_name_len)
+
+        if first_time:
+            logging.debug("Connected to '%s'.", tv_name.decode())
+
+        response_len = int.from_bytes(self.connection.recv(2),
+                                      byteorder="little")
+        response = self.connection.recv(response_len)
+
+        if len(response) == 0:
+            self.close()
+            raise exceptions.ConnectionClosed()
+
+        if response == b"\x64\x00\x01\x00":
+            logging.debug("Access granted.")
+            return
+        elif response == b"\x64\x00\x00\x00":
+            raise exceptions.AccessDenied()
+        elif response[0:1] == b"\x0a":
+            if first_time:
+                logging.warning("Waiting for authorization...")
+            return self._read_response()
+        elif response[0:1] == b"\x65":
+            logging.warning("Authorization cancelled.")
+            raise exceptions.AccessDenied()
+        elif response == b"\x00\x00\x00\x00":
+            logging.debug("Control accepted.")
+            return
+
+        raise exceptions.UnhandledResponse(response)
+
+    @staticmethod
+    def _serialize_string(string, raw = False):
+        if isinstance(string, str):
+            string = str.encode(string)
+
+        if not raw:
+            string = base64.b64encode(string)
+
+        return bytes([len(string)]) + b"\x00" + string

samsungctl/remote_websocket.py
@@ -1,0 +1,76 @@
+import base64
+import json
+import logging
+import socket
+import time
+
+from . import exceptions
+
+class RemoteWebsocket():
+    """Object for remote control connection."""
+
+    def __init__(self, config):
+        import websocket
+
+        if not config["port"]:
+            config["port"] = 8001
+
+        URL_FORMAT = "ws://{}:{}/api/v2/channels/samsung.remote.control?name={}"
+
+        """Make a new connection."""
+        self.connection = websocket.WebSocket()
+        self.connection.connect(URL_FORMAT.format(config["host"], config["port"],
+                                                  self._serialize_string(config["name"])))
+
+        self._read_response()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def close(self):
+        """Close the connection."""
+        if self.connection:
+            self.connection.close()
+            self.connection = None
+            logging.debug("Connection closed.")
+
+    def control(self, key):
+        """Send a control command."""
+        if not self.connection:
+            raise exceptions.ConnectionClosed()
+
+        payload = json.dumps({
+            "method": "ms.remote.control",
+            "params": {
+                "Cmd": "Click",
+                "DataOfCmd": key,
+                "Option": "false",
+                "TypeOfRemote": "SendRemoteKey"
+            }
+        })
+
+        logging.info("Sending control command: %s", key)
+        self.connection.send(payload)
+        time.sleep(self._key_interval)
+
+    _key_interval = 1.0
+
+    def _read_response(self):
+        response = self.connection.recv()
+        response = json.loads(response)
+
+        if response["event"] != "ms.channel.connect":
+            self.close()
+            raise exceptions.UnhandledResponse(response)
+
+        logging.debug("Access granted.")
+
+    @staticmethod
+    def _serialize_string(string):
+        if isinstance(string, str):
+            string = str.encode(string)
+
+        return base64.b64encode(string).decode("utf-8")

commit 88ea177070e772214abab550570bc84d1550713b

Restore Remote object context manager support
This was broken in e5f7f4420.

samsungctl/__main__.py
@@ -98,16 +98,15 @@ def main():
         return
 
     try:
-        remote = Remote(config)
+        with Remote(config) as remote:
+            for key in args.key:
+                remote.control(key)
 
-        for key in args.key:
-            remote.control(key)
-
-        if args.interactive:
-            logging.getLogger().setLevel(logging.ERROR)
-            interactive.run(remote)
-        elif len(args.key) == 0:
-            logging.warning("Warning: No keys specified.")
+            if args.interactive:
+                logging.getLogger().setLevel(logging.ERROR)
+                interactive.run(remote)
+            elif len(args.key) == 0:
+                logging.warning("Warning: No keys specified.")
     except exceptions.ConnectionClosed:
         logging.error("Error: Connection closed!")
     except exceptions.AccessDenied:

samsungctl/remote.py
@@ -11,6 +11,12 @@ class Remote:
         else:
             raise exceptions.UnknownMethod()
 
+    def __enter__(self):
+        return self.remote.__enter__()
+
+    def __exit__(self, type, value, traceback):
+        self.remote.__exit__(type, value, traceback)
+
     def close(self):
         return self.remote.close()
 