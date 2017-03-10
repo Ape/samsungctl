ef576a884bd4155fbc8bec8615c25544e74b98c3
Lauri Niskanen <ape@ape3000.com>
2017-03-10 09:29:38 +0200

    Use PEP 440 compatible local version identifier

--- samsungctl/__init__.py
+++ samsungctl/__init__.py
@@ -3,7 +3,7 @@
 from .remote import Remote
 
 __title__        = "samsungctl"
-__version__      = "0.6.0-git"
+__version__      = "0.6.0+1"
 __url__          = "https://github.com/Ape/samsungctl"
 __author__       = "Lauri Niskanen"
 __author_email__ = "ape@ape3000.com"
