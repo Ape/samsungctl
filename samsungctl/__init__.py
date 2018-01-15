d5e5af2688b19263a1207505978449d98a6f0b60
Lauri Niskanen <ape@ape3000.com>
2018-01-15 21:58:45 +0200

    Bump version to 0.7.1

--- samsungctl/__init__.py
+++ samsungctl/__init__.py
@@ -3,7 +3,7 @@
 from .remote import Remote
 
 __title__ = "samsungctl"
-__version__ = "0.7.0+1"
+__version__ = "0.7.1+1"
 __url__ = "https://github.com/Ape/samsungctl"
 __author__ = "Lauri Niskanen"
 __author_email__ = "ape@ape3000.com"
