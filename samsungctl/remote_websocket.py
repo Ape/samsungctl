0cf8896528c2d9609d337461b00873646c80ec74
Kevin Schlosser <drschlosser@hotmail.com>
2018-12-17 04:16:56 -0700

    websocket ssl only

--- samsungctl/remote_websocket.py
+++ samsungctl/remote_websocket.py
@@ -2,6 +2,7 @@ import base64
 import json
 import logging
 import socket
+import threading
 import time
 import ssl
 import os
@@ -11,31 +12,62 @@ from . import exceptions
 URL_FORMAT = "ws://{}:{}/api/v2/channels/samsung.remote.control?name={}"
 SSL_URL_FORMAT = "wss://{}:{}/api/v2/channels/samsung.remote.control?name={}"
 
+
 class RemoteWebsocket():
     """Object for remote control connection."""
 
     def __init__(self, config):
         import websocket
-        self.token_file = os.path.dirname(os.path.realpath(__file__)) + "/token.txt"
-        if not config["port"]:
-            config["port"] = 8001
+        import sys
 
+        if sys.platform.startswith('win'):
+            path = os.path.join('%appdata%', 'samsungctl')
+
+        else:
+            path = os.path.join('~', '.samsungctl')
+
+        path = os.path.expandvars(path)
+
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        token_file = path + "/token.txt"
+        if not os.path.exists(token_file):
+            with open(token_file, 'w') as f:
+                f.write('')
+
+        with open(token_file, 'r') as f:
+            tokens = f.read()
+
+        for line in tokens.split('\n'):
+            if line.startswith(config["host"]):
+                token = "&token=" + line.replace(config["host"] + ':', '')
+                break
+        else:
+            token = ''
+
+        self.token_file = token_file
+
+        config['port'] = 8002
         if config["timeout"] == 0:
             config["timeout"] = None
 
-        if config["port"] == 8002:
-            url = SSL_URL_FORMAT.format(config["host"], config["port"],
-                                        self._serialize_string(config["name"]))
-            if os.path.isfile(self.token_file):
-                with open(self.token_file, "r") as token_file:
-                    url += "&token=" + token_file.readline()
-            self.connection = websocket.create_connection(url, config["timeout"], sslopt={"cert_reqs": ssl.CERT_NONE})
+        url = SSL_URL_FORMAT.format(
+            config["host"],
+            config["port"],
+            self._serialize_string(config["name"])
+        ) + token
 
-        else:
-            url = URL_FORMAT.format(config["host"], config["port"],
-                                    self._serialize_string(config["name"]))
-            self.connection = websocket.create_connection(url, config["timeout"])
+        self.config = config
+        self.connection = websocket.create_connection(
+            url,
+            config["timeout"], sslopt={"cert_reqs": ssl.CERT_NONE}
+        )
+        self.auth_event = threading.Event()
         self._read_response()
+        if not self.auth_event.isSet():
+            self.close()
+            raise RuntimeError('Auth Failure')
 
     def __enter__(self):
         return self
@@ -67,6 +99,7 @@ class RemoteWebsocket():
 
         logging.info("Sending control command: %s", key)
         self.connection.send(payload)
+        self._read_response()
         time.sleep(self._key_interval)
 
     _key_interval = 0.5
@@ -74,16 +107,17 @@ class RemoteWebsocket():
     def _read_response(self):
         response = self.connection.recv()
         response = json.loads(response)
+        logging.debug(response)
 
-        if 'data' in response and 'token' in response["data"]:
-            with open(self.token_file, "w") as token_file:
-                token_file.write(response['data']["token"])
-
-        if response["event"] != "ms.channel.connect":
-            self.close()
-            raise exceptions.UnhandledResponse(response)
-
-        logging.debug("Access granted.")
+        if response["event"] == "ms.channel.connect":
+            if 'data' in response and 'token' in response["data"]:
+                with open(self.token_file, "a") as token_file:
+                    token_file.write(
+                        self.config['host'] + ':' + response['data'][
+                            "token"] + '\n'
+                    )
+                    logging.debug("Access granted.")
+                    self.auth_event.set()
 
     @staticmethod
     def _serialize_string(string):
